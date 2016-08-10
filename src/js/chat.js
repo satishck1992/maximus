77462ec1d71e996e2b17d921461c73750a29665f
Rahul Garg <rahul_garg08@yahoo.com>
2016-08-10 15:58:31 +0530

    added chat window html and css

--- src/js/chat.js
+++ src/js/chat.js
@@ -14,12 +14,52 @@ $('document').ready(function() {
     }
   }
 
-  connectToXMPP(CONFIG.service.url, CONFIG.service.protocol, CONFIG.user.jid, CONFIG.user.password);
+  var conn;
+
+  connectXMPPServer(CONFIG.service.url, CONFIG.service.protocol, CONFIG.user.jid, CONFIG.user.password)
+    .then(function(connection) {
+      conn= connection;
+      connectToGroup(conn, CONFIG.room.node, function() {})
+        .then(function (success) {
+          console.log('Connected to Group, Congratulations..')
+        }, function(error) {
+          console.log('Could not connect to the Group. Please try again later..');
+        });
+    }, function(error) {
+      console.log('Could not connect to XMPP Server. Please try later..');
+    });
 });
 
 /**
- * This function connects to the XMPP server.
+ * Connect To XMPP host via Strophe and promises.
+ * params
+ * 1. host -> XMPP service path.
+ * 2. protocol -> protocol for the Service
+ * 3. user_jid -> User JID for XMPP Server
+ * 4. user_password -> User Password for XMPP Server
  */
-function connectToXMPP() {
+function connectXMPPServer(host, protocol, user_jid, user_password) {
+      return new Promise(function (fulfill, reject) {
+            var conn = new Strophe.Connection(host, { protocol: protocol });
+            conn.connect(user_jid, user_password, function (response) {
+                  if (response === Strophe.Status.CONNECTED) {
+                        fulfill(conn);
+                  } else if (response === Strophe.Status.DISCONNECTED) {
+                        reject();
+                  }
+            });
+      });
+}
 
+/**
+ * Connect Connection to a Room node via Strophe Pubsub Library.
+ * params
+ * 1. conn -> Strophe Connection
+ * 2. room_node -> Room to connect to
+ * 3. subscribe_event (function) -> Function to handle events inside Room.
+ */
+function connectToGroup(conn, room_node, subscribe_event) {
+      return new Promise(function (fulfill, reject) {
+            conn.pubsub.subscribe(room_node, [], subscribe_event, fulfill, reject);
+      });
 }
