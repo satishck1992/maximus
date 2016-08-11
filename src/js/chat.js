8c6cd5130d7de718bfc019f9a1701b7675027a09
Rahul Garg <rahul_garg08@yahoo.com>
2016-08-11 11:30:45 +0530

    Autoformatted Code. Removed chat_messenger useless file.

--- src/js/chat.js
+++ src/js/chat.js
@@ -1,4 +1,4 @@
-$('document').ready(function() {
+$('document').ready(function () {
 
   var CONFIG = {
     'service': {
@@ -17,17 +17,17 @@ $('document').ready(function() {
   var conn;
 
   connectXMPPServer(CONFIG.service.url, CONFIG.service.protocol, CONFIG.user.jid, CONFIG.user.password)
-    .then(function(connection) {
-      conn= connection;
-    }, function(error) {
+    .then(function (connection) {
+      conn = connection;
+    }, function (error) {
       console.log('Could not connect to XMPP Server. Please try later..');
     });
 
-    $('.group_join--btn').click(function(e) {
-      var groupId= $(this).closest('li.group').data('group-id');
-      var groupTitle= $(this).closest('li.group').find('.detail .title').html();
-      joinGroup(conn, groupId, groupTitle, 'Lallan');
-    });
+  $('.group_join--btn').click(function (e) {
+    var groupId = $(this).closest('li.group').data('group-id');
+    var groupTitle = $(this).closest('li.group').find('.detail .title').html();
+    joinGroup(conn, groupId, groupTitle, 'Lallan');
+  });
 });
 
 /**
@@ -39,16 +39,16 @@ $('document').ready(function() {
  * 4. user_password -> User Password for XMPP Server
  */
 function connectXMPPServer(host, protocol, user_jid, user_password) {
-      return new Promise(function (fulfill, reject) {
-            var conn = new Strophe.Connection(host, { protocol: protocol });
-            conn.connect(user_jid, user_password, function (response) {
-                  if (response === Strophe.Status.CONNECTED) {
-                        fulfill(conn);
-                  } else if (response === Strophe.Status.DISCONNECTED) {
-                        reject();
-                  }
-            });
-      });
+  return new Promise(function (fulfill, reject) {
+    var conn = new Strophe.Connection(host, { protocol: protocol });
+    conn.connect(user_jid, user_password, function (response) {
+      if (response === Strophe.Status.CONNECTED) {
+        fulfill(conn);
+      } else if (response === Strophe.Status.DISCONNECTED) {
+        reject();
+      }
+    });
+  });
 }
 
 /**
@@ -60,7 +60,7 @@ function connectXMPPServer(host, protocol, user_jid, user_password) {
  */
 function joinGroup(conn, room_node, room_name, user_name) {
 
-  var Messenger= {
+  var Messenger = {
     conn: conn,
     room_node: room_node,
     room_name: room_name,
@@ -70,32 +70,32 @@ function joinGroup(conn, room_node, room_name, user_name) {
       'messageList': '',
       'textInput': ''
     },
-    init: function() {
-      var self= this;
+    init: function () {
+      var self = this;
 
       this.addChatWindow();
       this.connectToGroup()
-        .then(function(success) {
+        .then(function (success) {
           self.bindEvents();
-        }, function(error) {
+        }, function (error) {
           console.log('Sorry, could not connect to group');
         });
     },
-    addChatWindow: function() {
+    addChatWindow: function () {
       // if already window is present
-      if($('.chat-windows-wrapper').find('div[data-group-id="'+this.room_node+'"]').length) {
-        return ;
+      if ($('.chat-windows-wrapper').find('div[data-group-id="' + this.room_node + '"]').length) {
+        return;
       }
-      var html= '';
-      html+= '<div class="box chat-window" data-group-id="'+this.room_node+'">';
-        html+= '<div class="header"><i class="material-icons">chat</i><div class="title">'+this.room_name+'</div><a href="#" class="close-chat"><i class="material-icons">clear</i></a></div>';
-        html+= '<ul class="chat-messages"></ul>';
-        html+= '<div class="send-message"><i class="material-icons emotions-icon">insert_emoticon</i><input type="text" placeholder="Start Chatting"><i class="material-icons attach-icon">wb_cloudy</i></div>';
-      html+= '</div>';
+      var html = '';
+      html += '<div class="box chat-window" data-group-id="' + this.room_node + '">';
+      html += '<div class="header"><i class="material-icons">chat</i><div class="title">' + this.room_name + '</div><a href="#" class="close-chat"><i class="material-icons">clear</i></a></div>';
+      html += '<ul class="chat-messages"></ul>';
+      html += '<div class="send-message"><i class="material-icons emotions-icon">insert_emoticon</i><input type="text" placeholder="Start Chatting"><i class="material-icons attach-icon">wb_cloudy</i></div>';
+      html += '</div>';
       $('.chat-windows-wrapper').append(html);
-      this.domEl.closeButton= $('.chat-windows-wrapper').find('div[data-group-id="'+this.room_node+'"] a.close-chat');
-      this.domEl.messageList= $('.chat-windows-wrapper').find('div[data-group-id="'+this.room_node+'"] ul.chat-messages');
-      this.domEl.textInput= $('.chat-windows-wrapper').find('div[data-group-id="'+this.room_node+'"] input[type="text"]');
+      this.domEl.closeButton = $('.chat-windows-wrapper').find('div[data-group-id="' + this.room_node + '"] a.close-chat');
+      this.domEl.messageList = $('.chat-windows-wrapper').find('div[data-group-id="' + this.room_node + '"] ul.chat-messages');
+      this.domEl.textInput = $('.chat-windows-wrapper').find('div[data-group-id="' + this.room_node + '"] input[type="text"]');
     },
     /**
    * Connect Connection to a Room node via Strophe Pubsub Library.
@@ -104,46 +104,46 @@ function joinGroup(conn, room_node, room_name, user_name) {
    * 2. room_node -> Room to connect to
    * 3. subscribe_event (function) -> Function to handle events inside Room.
    */
-    connectToGroup: function() {
-      var self= this;
+    connectToGroup: function () {
+      var self = this;
       return new Promise(function (fulfill, reject) {
         self.conn.pubsub.subscribe(self.room_node, [], self.subscribe_event, fulfill, reject);
       });
     },
-    subscribe_event: function(message) {
+    subscribe_event: function (message) {
       var _data = $(message).children('event')
         .children('items')
         .children('item').text();
 
       var parsedDecodedData = JSON.parse(decodeURIComponent(_data));
-      var user_from= parsedDecodedData.message_from;
+      var user_from = parsedDecodedData.message_from;
       var html = '';
-      if(user_from=== Messenger.user_name) {
+      if (user_from === Messenger.user_name) {
         html += '<li class="chat-message us">';
       } else {
         html += '<li class="chat-message them">';
       }
-        html+= parsedDecodedData.message_text_data;
+      html += parsedDecodedData.message_text_data;
       html += '</li>';
       $(Messenger.domEl.messageList).append(html);
-      Messenger.domEl.messageList.find('li:last-child').velocity("scroll", { 
+      Messenger.domEl.messageList.find('li:last-child').velocity("scroll", {
         container: Messenger.domEl.messageList,
         duration: 100
       });
       return true;
     },
-    bindEvents: function() {
-      var self= this;
-      $(this.domEl.textInput).keyup(function(e) {
-        if(e.keyCode=== 13) {
+    bindEvents: function () {
+      var self = this;
+      $(this.domEl.textInput).keyup(function (e) {
+        if (e.keyCode === 13) {
           self.sendMessage();
         }
       });
-      $(this.domEl.closeButton).click(function(e) {
+      $(this.domEl.closeButton).click(function (e) {
         self.removeConnection();
       });
     },
-    sendMessage: function() {
+    sendMessage: function () {
       var textMessage = $(this.domEl.textInput).val();
       if (textMessage) {
         var op = {
@@ -160,22 +160,22 @@ function joinGroup(conn, room_node, room_name, user_name) {
         $(this.domEl.textInput).val('');
       }
     },
-    removeConnection: function() {
+    removeConnection: function () {
       this.unbindEvents();
       this.closeConnection();
       this.removeDomElement();
     },
-    unbindEvents: function() {
+    unbindEvents: function () {
       $(this.domEl.textInput).unbind();
       $(this.domEl.closeButton).unbind();
     },
-    closeConnection: function() {
+    closeConnection: function () {
       this.conn.pubsub.unsubscribe(this.room_node, this.user_name);
     },
-    removeDomElement: function() {
-      $('.chat-windows-wrapper').find('div[data-group-id="'+this.room_node+'"]').remove();
+    removeDomElement: function () {
+      $('.chat-windows-wrapper').find('div[data-group-id="' + this.room_node + '"]').remove();
     }
   }
-  
+
   Messenger.init();
 }
--- src/js/login.js
+++ src/js/login.js
@@ -1,15 +1,15 @@
-$('document').ready(function() {
+$('document').ready(function () {
 
   // capture login form submit event.
-  $('#login-form').submit(function(e) {
+  $('#login-form').submit(function (e) {
     e.preventDefault();
-    var user_id= $("#user_name").val();
-    var password= $("#password").val();
-    var isAuth= authenticate(user_id, password);
-    if(!isAuth) {
+    var user_id = $("#user_name").val();
+    var password = $("#password").val();
+    var isAuth = authenticate(user_id, password);
+    if (!isAuth) {
       $(".result").html('Invalid User');
     } else {
-      window.location.href= 'chat.html';
+      window.location.href = 'chat.html';
     }
   });
 });
@@ -19,8 +19,8 @@ $('document').ready(function() {
  * Function to authenticate User.
  */
 function authenticate(user_name, password) {
-  var HARD_CODED_USER= {user_name: 'admin', password: 'admin'};
-  if(user_name=== HARD_CODED_USER.user_name && password=== HARD_CODED_USER.password) {
+  var HARD_CODED_USER = { user_name: 'admin', password: 'admin' };
+  if (user_name === HARD_CODED_USER.user_name && password === HARD_CODED_USER.password) {
     return true;
   } else {
     return false;
